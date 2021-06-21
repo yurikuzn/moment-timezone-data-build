3e52702f66818b38e22b620d92b68c8a8748467c
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2021-06-21 17:00:48 +0300

    release and pack

--- build.js
+++ build.js
@@ -1,20 +1,20 @@
-var fs = require('fs');
-
-const lastYear = process.argv[2] || 2030;
-
+const fs = require('fs');
 const fullData = require('./latest.json');
-
 const moment = require('./node_modules/moment/moment.js');
 const momentTimizone = require('./node_modules/moment-timezone/moment-timezone.js');
 const utils = require('./node_modules/moment-timezone/moment-timezone-utils.js');
 
+const lastYear = process.argv[2] || 2032;
+
 const targetData = moment.tz.filterLinkPack(fullData, 1970, lastYear);
 
-var code = `
-moment.tz.load(`+JSON.stringify(targetData, true)+`)
-`;
+let code = `moment.tz.load(` + JSON.stringify(targetData, true) + `);`;
 
-var resFilePath = './build/moment-timezone-data.js';
+let resFilePath = './build/moment-timezone-data.js';
+
+if (!fs.existsSync('./build')) {
+    fs.mkdirSync('./build');
+}
 
 if (fs.existsSync(resFilePath)) {
     fs.unlinkSync(resFilePath);
